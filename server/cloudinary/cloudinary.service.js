be355783fc606a35fb3e0cceb381c30686f7b6a2
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-06 01:06:19 +0900

    Change Cloudinaryの接続方法をCLOUDINARY_URLから取得するようにした
    デフォルトでCLOUDINARY_URLから取得していて、Herokuの環境設定上に追加の設定が必要ないため

--- server/cloudinary/cloudinary.service.js
+++ server/cloudinary/cloudinary.service.js
@@ -1,12 +1,6 @@
 var config = require('../config/environment');
 var cloudinary = require('cloudinary');
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SEACRET
-});
-
 function upload(file) {
   return cloudinary.uploader.upload(file);
 }
